4239825aa7894f5a47f4a373b9ac7dddd9f7ff40
zhuola01 <zhuola01@mingyuanyun.com>
2023-08-22 17:33:20 +0800

    terrible grammer

--- gollection.go
+++ gollection.go
@@ -1,19 +1,37 @@
 package gollection
 
-type Collection[T any] struct {
+type Collection[T interface{}] struct {
 	items []T
 }
 
-func NewCollection[T any](items []T) Collection[T] {
-	return Collection[T]{items: items}
+func NewCollection[T any](items []T) *Collection[T] {
+	return &Collection[T]{items: items}
 }
 
-func (c *Collection[T]) Add(item T) {
+func (c *Collection[T]) Add(item T) *Collection[T] {
 	c.items = append(c.items, item)
+	return c
 }
 
-func (c *Collection[T]) Each(action func(T)) {
+func (c *Collection[T]) Each(action func(T)) *Collection[T] {
 	for _, item := range c.items {
 		action(item)
 	}
+	return c
+}
+
+func (c *Collection[T]) Map(f func(T) any) interface{} {
+	result := NewCollection[any]([]any{})
+	for _, item := range c.items {
+		result.Add(f(item))
+	}
+	return result
+}
+
+func Map[T any, TT any](c *Collection[T], f func(T) TT) *Collection[TT] {
+	result := make([]TT, len(c.items))
+	for i, item := range c.items {
+		result[i] = f(item)
+	}
+	return &Collection[TT]{result}
 }
--- test/gollection_test.go
+++ test/gollection_test.go
@@ -1,7 +1,17 @@
 package test
 
-import "testing"
+import (
+	"fmt"
+	"github.com/ansuir/gollection"
+	"testing"
+)
 
 func TestGollection(t *testing.T) {
+
+	gollection.NewCollection([]int{1, 2, 3}).Map(func(i int) interface{} {
+		return fmt.Sprintf("I is %d", i)
+	}).(*gollection.Collection[any]).Each(func(s any) {
+		fmt.Printf("S is '%s' \n", s.(string))
+	})
 
 }
